8efe44fddb74a16c494a903ffb107cbfd906c236
Deluxe2186 <yanmife.arayela@gmail.com>
2025-03-24 23:38:23 +0100

    Week_2

--- Week-2/project_2.py
+++ Week-2/project_2.py
@@ -6,3 +6,19 @@ print("2. Cubic equation")
 print("3. Quartic equation")
 choice = int(input("Enter your choice: "))
 print(f"you have chosen {eqn[choice-1]} equation")
+if choice == 1:
+    a = float(input('Enter a value for the coefficient of X^2'))
+    b = float(input('Enter a value for the coefficient of X'))
+    c = float(input('Enter a value for the constant'))
+    d = (b**2) - (4*a*c)
+    if d < 0:
+        print("No real roots")
+    elif d == 0:
+        x = (-b) / (2*a)
+        print(f"the root is {x}")
+    else:
+        x1 = (-b + d**0.5) / (2*a)
+        x2 = (-b - d**0.5) / (2*a)
+        print(f"the roots are {x1} and {x2}")
+elif choice != 1:
+    print("This equation is not supported yet")
